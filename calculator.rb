ea9ec5f0ceb58620d5f9480c270df5994ec780e5
Clayton Lengel-Zigich <Clayton Lengel-Zigich clayton@claytonlz.com>
2011-08-18 08:41:38 -0700

    A simpler version

--- calculator.rb
+++ calculator.rb
@@ -2,17 +2,4 @@ class Calculator
   def add(x,y)
     x + y
   end
-  
-  def divide(x,y)
-    return "You can't divide by zero!" if y == 0
-    x / y
-  end
-  
-  def subtract(x,y)
-    y - x
-  end
-
-  def multiply(x,y)
-    x * y
-  end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -21,23 +21,4 @@ describe Calculator do
       end
     end
   end
-  context "Division" do
-    context "When I divide by zero" do
-      it "should give me a warning" do
-        @calc.divide(5,0).should == "You can't divide by zero!"
-      end
-    end
-    context "When I divide 10 by 2" do
-      it "should return 5 " do
-        @calc.divide(10,2).should == 5
-      end
-    end
-  end
-  context "Subtraction" do
-    context "When I subtract 5 from 10" do
-      it "should return 5" do
-        @calc.subtract(5,10).should == 5
-      end
-    end
-  end
 end
